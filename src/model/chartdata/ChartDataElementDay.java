bc523a1541e248893b4f871279d7eca9027c1012
sjs40 <samserio07@gmail.com>
2018-02-11 15:28:51 -0500

    chart data elements for day and month/year

---
+++ src/model/chartdata/ChartDataElementDay.java
@@ -0,0 +1,37 @@
+package model.chartdata;
+
+import org.json.JSONObject;
+
+public class ChartDataElementDay {
+
+  private String date;
+  private String minute;
+  private String label;
+  private double high;
+  private double low;
+  private double average;
+  private int volume;
+  private int numOfTrades;
+  private double change;
+
+  public ChartDataElementDay(JSONObject json) {
+    date = json.getString("date");
+    minute = json.getString("minute");
+    label = json.getString("label");
+    high = json.getDouble("high");
+    low = json.getDouble("low");
+    average = json.getDouble("average");
+    volume = json.getInt("volume");
+    numOfTrades = json.getInt("numberOfTrades");
+    change = json.getDouble("changeOverTime");
+  }
+
+  public double getY() {
+    return average;
+  }
+
+  public double getX() {
+    return Double.parseDouble(minute);
+  }
+
+}
---
+++ src/model/chartdata/ChartDataElementMY.java
@@ -0,0 +1,7 @@
+package model.chartdata;
+
+public class ChartDataElementMY {
+
+
+
+}
